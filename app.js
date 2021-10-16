fd4db2980a06b9f888f4282d158309cd3ce5ee55
davidh <dh@ptica.com>
2021-10-16 23:57:14 +0200

    added mongoose and created a db connection

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+let mongoose = require('mongoose');
+let mongoDB = 'mongodb+srv://david:librarypassword@local-library.ul20l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
