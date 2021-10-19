7e7af4e4f64baaef5d1a9a7d07d4659d3da21f77
david <dh@ptica.com>
2021-10-19 21:02:54 +0200

    added formatting to date strings

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 let mongoose = require('mongoose');
+let {DateTime} = require('luxon');
 
 let Schema = mongoose.Schema;
 
@@ -14,6 +15,13 @@ BookInstanceSchema
 .virtual('url')
 .get(function() {
     return '/catalog/bookinstance/' + this._id;
+});
+
+// Virtual for formatted dates
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function() {
+    return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
