3cfff15a9203f4a196a4270bd28026fe180e19a9
david <dh@ptica.com>
2021-11-19 20:40:23 +0100

    added form for genre creation (get, post); bugfix: name of genre not showing when on genre_detail page ->updated query to findOne

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,8 @@
 let Genre = require('../models/genre');
 let Book = require('../models/book');
 
+const {body, validationResult} = require('express-validator');
+
 // Display list of all Genre.
 let genre_list = function(req, res, next) {
     Genre.find()
@@ -13,14 +15,14 @@ let genre_list = function(req, res, next) {
 
 // Display detail page for a specific Genre.
 let genre_detail = async function(req, res, next) {
-    let genre = await Genre.find({name: req.params.id});
+    let genre = await Genre.findOne({name: req.params.id});
     if(genre === null) {
         let err = new Error('Genre not found');
         err.status = 404;
         return next(err);
     }
 
-    let genreBooks = Book.find({genre: genre[0]._id});
+    let genreBooks = Book.find({genre: genre._id});
     
     genreBooks
         .then(books => {
@@ -32,14 +34,46 @@ let genre_detail = async function(req, res, next) {
 };
 
 // Display Genre create form on GET.
-genre_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create GET');
+let genre_create_get = function(req, res, next) {
+    res.render('genre_form', {title: 'Create Genre'});
 };
 
 // Handle Genre create on POST.
-genre_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create POST');
-};
+let genre_create_post = [
+    //Validate and sanitize name field
+    body('name', 'Genre name required').trim().isLength({min: 1}).escape(),
+
+    //Process request
+    (req, res, next) => {
+        //Extract validaton errors
+        const errors = validationResult(req);
+
+        //create genre obj
+        let genre = new Genre({name: req.body.name});
+
+        if(!errors.isEmpty()) {
+            //Render form again with sanitizied values/errors
+            res.render('genre_form', {title: 'Create Genre', genre: genre, errors: errors.array()});
+            return;
+        }
+
+        // Data from form is valid
+        // Check if genre already exists
+        Genre.findOne({name: req.body.name})
+            .then(foundGenre => {
+                if(foundGenre) {
+                    res.redirect(foundGenre.url);
+                } else {
+                    genre.save()
+                    .then(savedGenre => {
+                            //genre saved
+                            res.redirect(genre.url);
+                    })
+                }
+            })
+            .catch(err => next(err));
+    }
+];
 
 // Display Genre delete form on GET.
 genre_delete_get = function(req, res) {
