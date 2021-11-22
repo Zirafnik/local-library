f5aad7d836f30827b46f832973dd89154892a6c2
david <dh@ptica.com>
2021-11-22 21:28:00 +0100

    added update logic for books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -142,12 +142,97 @@ book_delete_post = function(req, res) {
 };
 
 // Display book update form on GET.
-book_update_get = function(req, res) {
+let book_update_get = async function(req, res, next) {
+    let [book, authors, genres] = await Promise.all([
+        Book.findById(req.params.id).populate('author').populate('genre'),
+        Author.find(),
+        Genre.find()
+    ]).catch(err => next(err));
+
+    if(book === null) {
+        let err = new Error('Book not found');
+        err.status = 404;
+        return next(err);
+    }
+
+    // Success
+    // Mark selected genres
+    for(let i = 0; i < genres.length; i++) {
+        for(let j = 0; j < book.genre.length; j++) {
+            if(genres[i]._id.toString() === book.genre[j]._id.toString()) {
+                genres[i].checked = 'true';
+            }
+        }
+    }
+
+    res.render('book_form', {title: 'Update Book', authors, genres, book});
 };
 
 // Handle book update on POST.
-book_update_post = function(req, res) {
-};
+let book_update_post = [
+    //SAME AS CREATE POST (almost)
+    //turn genres into arr
+    (req, res, next) => {
+        if(!(req.body.genre instanceof Array)) {
+            if(!req.body.genre) {
+                req.body.genre = [];
+            } else {
+                req.body.genre = [req.body.genre];
+            }
+        }
+        next();
+    },
+
+    // Validate and sanitize
+    body('title', 'Title must not be empty.').trim().isLength({min: 1}).escape(),
+    body('author', 'Author must not be empty.').trim().isLength({min: 1}).escape(),
+    body('summary', 'Summary must not be empty.').trim().isLength({min: 1}).escape(),
+    body('isbn', 'ISBN must not be empty.').trim().isLength({min: 1}).escape(),
+    body('genre.*').escape(),
+
+    // Process request
+    async (req, res, next) => {
+        const errors = validationResult(req);
+
+        //errors present
+        if(!errors.isEmpty()) {
+            //again get all authors and genres to re-render the form
+            let authors = Author.find({});
+            let genres = Genre.find({});
+            
+            let results = await Promise.all([authors, genres])
+            .catch(err => next(err));
+
+            //mark selected genres as checked
+            for(let i = 0; i < results[1].length; i++) {
+                if(req.body.genre.indexOf(results[1][i]._id) > -1) {
+                    results[1][i].checked = 'true';
+                }
+            }
+            
+            res.render('book_form', {title: 'Update Book', authors: results[0], genres: results[1], book: req.body, errors: errors.array()});
+            return;
+        } 
+        //no errors
+        else {
+            //create book
+            let book = new Book({
+                title: req.body.title,
+                author: req.body.author,
+                summary: req.body.summary,
+                isbn: req.body.isbn,
+                genre: req.body.genre,
+                _id: req.params.id //This is required, or a new ID will be assigned!
+            });
+
+            Book.findByIdAndUpdate(req.params.id, book, {})
+                .then(thebook => {
+                    res.redirect(thebook.url);
+                })
+                .catch(err => next(err));
+        }
+    }
+];
 
 module.exports = {
     index,
